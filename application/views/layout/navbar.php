6642b2ab90bdbc37735f08f8fd6e7d351e859f44
oktadha01 <oktadha01@gmail.com>
2023-01-12 16:29:46 +0700

    navbar

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -2,7 +2,8 @@
    <div id="navbar-top" class="container-fluid d-flex align-items-center justify-content-between pr-var">
 
       <a href="https://www.instagram.com/pt.kanpa/?hl=en" class="logo d-flex align-items-center scrollto me-auto mr-0">
-         <i class="fa-brands fa-instagram" style="color: #00000085;font-size: 35px;"></i>
+         <!-- <i class="fa-brands fa-instagram" style="color: #00000085;font-size: 35px;"></i> --> 
+         <img src="<?php echo base_url('assets'); ?>/img/Logo PT Fix Bingit.png" class="" alt="" style="max-height: 30px;">
       </a>
       <a href="<?php echo base_url(); ?>#home" class="logo d-flex align-items-center scrollto me-auto mr-0">
          <!-- Uncomment the line below if you also wish to use an image logo -->

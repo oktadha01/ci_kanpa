fcb60b7ae5e925f7a4b24d7d4f20d03efce9d484
oktadha01 <oktadha01@gmail.com>
2023-03-29 16:42:38 +0700

    index

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -16,6 +16,7 @@
     </title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="google-site-verification" content="Da0TUaYScK7AIiQsOyTgtDTpMIBgIFtz3Gb7zkltBB4" />
+    <meta name="msvalidate.01" content="B36B1215CB3BC26AA0E6851087FF5E2F"   />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="x-dns-prefetch-control" content="on">
     <!-- Meta untuk SEO -->
